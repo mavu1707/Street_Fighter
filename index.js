9f041ab40d20459efa8c0196f16e61117e75d80c
Vullum <markus.yung.vullum@gmail.com>
2022-11-01 13:33:20 +0100

    fikser

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ canvas.height = 600
 //Gir canvas en context
 const ctx = canvas.getContext('2d')
 
-const gravity = 0.2
-
 //Lagrer informasjonen til player1 og player 2 i en array
 const player1 ={
   width: 50,
@@ -24,15 +22,16 @@ const player2 ={
   x: 800,
   y: 500,
   color: "green",
-  speed: 3
+  speed: 3,
+  isAttacking: false
 }
 
-const attack1 ={
+const attackBox1 ={
   width: 100,
   height: 50
 }
 
-const attack2 ={
+const attackBox2 ={
   width: 100,
   height: 50
 }
@@ -52,36 +51,27 @@ function drawPlayer2(){
 }
 
 //Funksjon for attack boksene til player 1 og 2
-function player1Attack(){
+function drawAttackBox1(){
   ctx.beginPath()
   ctx.fillStyle = "black"
-  ctx.fillRect(player1.x, player1.y, attack1.width, attack1.height)
+  ctx.fillRect(player1.x, player1.y, attackBox1.width, attackBox1.height)
   ctx.closePath()
 }
-function player2Attack(){
+function drawAttackBox2(){
   ctx.beginPath()
   ctx.fillStyle = "blue"
-  ctx.fillRect(player2.x, player2.y, attack2.width, attack2.height)
+  ctx.fillRect(player2.x, player2.y, attackBox2.width, attackBox2.height)
   ctx.closePath()
 }
 
-function player1IsAttacking(){
-  player1.isAttacking = true
-  setTimeout(() =>{
-    player1.isAttacking = false
-  }, 100)
-}
 //Player 1 sin movement
 let right = false
 let left = false
 let down = false
-
 function checkButton(event) {
   event.key === "a" ? left = true : null
   event.key === "d" ? right = true : null
-  event.key === "s" ? player1Attack = true : null
 }
-
 document.addEventListener("keydown", checkButton)
 document.addEventListener("keyup", event => {
   event.key === "a" ? left = false : null
@@ -91,19 +81,17 @@ document.addEventListener("keyup", event => {
 //Player 2 sin movement
 let righty = false
 let lefty = false
-
 function check(event) {
   event.key === "ArrowLeft" ? lefty = true : null
   event.key === "ArrowRight" ? righty = true : null
 }
-
-//Lytte etter tastetrykk:
 document.addEventListener("keydown", check)
 document.addEventListener("keyup", event => {
   event.key === "ArrowLeft" ? lefty = false : null
   event.key === "ArrowRight" ? righty = false : null
 })
 
+//funksjon som tegner opp alle elementer
 function drawElements(){
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 
@@ -111,17 +99,17 @@ function drawElements(){
   left ? player1.x = player1.x - player1.speed : null
   right ? player1.x = player1.x + player1.speed : null
 
-
-  player1Attack()
-  player2Attack()
+  drawAttackBox1()
+  drawAttackBox2()
 
   drawPlayer2()
   lefty ? player2.x = player2.x - player2.speed : null
   righty ? player2.x = player2.x + player2.speed : null
 
-  if (player1.x + attack1.width >= player2.x &&
-    player1.x < player2.x + player2.width &&
-    player1.isAttacking){
+  if (
+    player1.x + attackBox1.width >= player2.x &&
+    player1.x < player2.x + player2.width
+    ){
     console.log("hei");
   }
 
