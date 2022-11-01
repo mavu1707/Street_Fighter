60bf69bdde6901513ef17196c777ded2486f0f4d
Vullum <markus.yung.vullum@gmail.com>
2022-11-01 12:34:40 +0100

    angrep funker

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ canvas.height = 600
 //Gir canvas en context
 const ctx = canvas.getContext('2d')
 
+const gravity = 0.2
+
 //Lagrer informasjonen til player1 og player 2 i en array
 const player1 ={
   width: 50,
@@ -21,7 +23,17 @@ const player2 ={
   x: 800,
   y: 500,
   color: "green",
-  speed: 2
+  speed: 3
+}
+
+const attack1 ={
+  width: 100,
+  height: 50
+}
+
+const attack2 ={
+  width: 100,
+  height: 50
 }
 
 //Funksjon for player 1 og 2 sine posisjoner og farger
@@ -35,6 +47,20 @@ function drawPlayer2(){
   ctx.beginPath()
   ctx.fillStyle = player2.color
   ctx.fillRect(player2.x, player2.y, player2.width, player2.height)
+  ctx.closePath()
+}
+
+//Funksjon for attack boksene til player 1 og 2
+function player1Attack(){
+  ctx.beginPath()
+  ctx.fillStyle = "black"
+  ctx.fillRect(player1.x, player1.y, attack1.width, attack1.height)
+  ctx.closePath()
+}
+function player2Attack(){
+  ctx.beginPath()
+  ctx.fillStyle = "blue"
+  ctx.fillRect(player2.x, player2.y, attack2.width, attack2.height)
   ctx.closePath()
 }
 
@@ -76,13 +102,21 @@ function drawElements(){
   left ? player1.x = player1.x - player1.speed : null
   right ? player1.x = player1.x + player1.speed : null
 
+
+  player1Attack()
+  player2Attack()
+
   drawPlayer2()
   lefty ? player2.x = player2.x - player2.speed : null
   righty ? player2.x = player2.x + player2.speed : null
 
+  if (player1.x + attack1.width >= player2.x &&
+    player1.x < player2.x + player2.width){
+    console.log("hei");
+  }
+
   requestAnimationFrame(drawElements)
 }
-
 drawElements()
 
 
